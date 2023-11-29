260206a1db5292883e23f5c7d54bceef2c7eeebd
shavvimal <shavhugan@gmail.com>
2023-11-29 21:22:19 +0000

    pandoc scripts

---
+++ pandoc.sh
@@ -0,0 +1,35 @@
+pandoc \
+-t markdown \
+--extract-media='./img' \
+doc.docx \
+--wrap=none \
+-o doc.md
+
+# ==============================================================================
+
+#!/bin/bash
+# Directory containing the EPUB files
+SOURCE_DIR="./epubs_new"
+# Directory where the Markdown files will be saved
+TARGET_DIR="./markdowns_new"
+
+# Function to process a single file
+process_file() {
+    epub="$1"
+    filename=$(basename -- "$epub")
+    base_name="${filename%.*}"
+    # Create a directory for extracted media specific to this EPUB file
+    mkdir -p "$TARGET_DIR/$base_name/img"
+    # Convert EPUB to Markdown, placing extracted media in the created directory
+    pandoc -t markdown --extract-media="$TARGET_DIR/$base_name/img" "$epub" --wrap=none -o "$TARGET_DIR/$base_name.md"
+    echo "$filename converted."
+}
+
+export -f process_file
+export SOURCE_DIR
+export TARGET_DIR
+# Find all EPUB files and pass them to parallel
+# sudo apt-get install parallel
+find "$SOURCE_DIR" -name "*.epub" | parallel process_file
+echo "All conversions completed."
+
