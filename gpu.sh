b694fddd9edaa90cd7e476282fb8293e45343c54
Shav Vimalendiran <shavhugan@gmail.com>
2024-08-11 22:24:09 +0100

    gpu

---
+++ gpu.sh
@@ -0,0 +1,6 @@
+# To see the GPU's working, run `wsl -list` to see available Linux distributions. 
+# Use `wsl -d Ubuntu` to connect to the Ubuntu instance. 
+# Run this command to watch the performace of the Nvidia GPU's in the terminal:
+
+watch -n 0.5 nvidia-smi
+
